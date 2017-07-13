a209901e0b9f5182e2c857dc9ff2b9fabf392d30
Tetsuya Mori <monry84@gmail.com>
2017-07-13 15:36:36 +0900

    :white_check_mark: テストコード追加
    * が、これは Editor で動かないという致命的な問題を抱えている。
    * GameObject の描画画角を変更しないとダメなんだけど、外部から弄れないっぽいんだよね...

--- Assets/Tests/Scripts/CanvasResizerTest.cs
+++ Assets/Tests/Scripts/CanvasResizerTest.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using GameObjectExtension;
+﻿using GameObjectExtension;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 using UnityEngine.UI;
 
 namespace Tests {
@@ -13,14 +11,39 @@ namespace Tests {
 
         [Test]
         public void ResizeTest() {
-            Assert.IsTrue(this.go.HasComponent<CanvasResizer>(), "CanvasResizer がちゃんと AddComponent されている");
+            // FIXME: これは、必ずどこかで転ける。 Screen.SetResolution ではエディタの GameView の解像度を弄れないため。
+
+            Screen.SetResolution(1136, 640, false);
+            this.go.GetComponent<CanvasResizer>().Resize();
+            Assert.IsTrue(
+                Mathf.Approximately(this.go.GetComponent<CanvasScaler>().matchWidthOrHeight, 0.0f),
+                "iPhone 7"
+            );
+
+            Screen.SetResolution(2048, 1536, false);
+            this.go.GetComponent<CanvasResizer>().Resize();
+            Assert.IsTrue(
+                Mathf.Approximately(this.go.GetComponent<CanvasScaler>().matchWidthOrHeight, 1.0f),
+                "iPad"
+            );
+
+            Screen.SetResolution(1024, 640, false);
+            this.go.GetComponent<CanvasResizer>().Resize();
+            Assert.IsTrue(
+                Mathf.Approximately(this.go.GetComponent<CanvasScaler>().matchWidthOrHeight, 0.0f),
+                "Android"
+            );
         }
 
         [SetUp]
         public void Setup() {
             this.go = new GameObject();
             this.go.AddComponent<Canvas>();
-            this.go.AddComponent<CanvasScaler>();
+            this.go.AddComponent<CanvasScaler>(
+                (x) => {
+                    x.referenceResolution = new Vector2(2272.0f, 1536.0f);
+                }
+            );
             this.go.AddComponent<CanvasResizer>();
         }
 
