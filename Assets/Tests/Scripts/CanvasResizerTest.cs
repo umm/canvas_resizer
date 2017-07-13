15ca2f943af570a8be13bff07ad2bacb7b714724
Tetsuya Mori <monry84@gmail.com>
2017-07-13 14:14:53 +0900

    :new: [WIP] 最低限ひな形を作っておく

---
+++ Assets/Tests/Scripts/CanvasResizerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests {
+
+    public class CanvasResizerTest {
+
+        [UnityTest]
+        public void ResizeTest() {
+            GameObject go = new GameObject();
+            go.AddComponent<Canvas>();
+            CanvasScaler canvasScaler = go.GetComponent<CanvasScaler>();
+
+            go.AddComponent<CanvasResizer>();
+        }
+
+    }
+
+}
